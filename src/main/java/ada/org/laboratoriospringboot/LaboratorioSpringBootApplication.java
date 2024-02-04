6e1b0b4c88c8741a9e0df3a77dab9807bcd8625f
SebastianAlfonsoMartinez <sebastianalfonso94@gmail.com>
2024-02-04 06:33:43 -0500

    Proyecto Integrador: configuración inicial Booking System REST API.

---
+++ src/main/java/ada/org/laboratoriospringboot/LaboratorioSpringBootApplication.java
@@ -0,0 +1,13 @@
+package ada.org.laboratoriospringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LaboratorioSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LaboratorioSpringBootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ada/org/laboratoriospringboot/LaboratorioSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package ada.org.laboratoriospringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LaboratorioSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
